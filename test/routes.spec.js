3c7ae2b519082fa5cd97214133355d3078b50bab
Riza Fahmi <rizafahmi@gmail.com>
2016-11-13 06:48:13 +0700

    Test configuration

---
+++ test/routes.spec.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+// *** GET all shows *** //
+router.get('/shows', (req, res, next) {
+  res.send('send shows back')
+})
+
+module.exports = router
