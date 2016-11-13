ac9d64cdd0c7872a49d96a4de4327cb87945a28a
Riza Fahmi <rizafahmi@gmail.com>
2016-11-13 07:43:42 +0700

    Add beforeEach and afterEach for isolated test

--- test/shows.test.js
+++ test/shows.test.js
@@ -4,28 +4,48 @@ const chai = require('chai')
 const should = chai.should()
 const chaiHttp = require('chai-http')
 const server = require('../app.js')
+const knex = require('../db/knex.js')
 
 
 chai.use(chaiHttp)
 
-describe('GET /api/v1/shows', () => {
-  it('should return all shows', (done) => {
-    chai.request(server)
-      .get('/api/v1/shows')
-      .end((err, res) => {
-        res.should.have.status(200)
-        res.should.be.json
-        res.body.should.be.a('array')
-        res.body.length.should.equal(4)
-        res.body[0].should.have.property('name')
-        res.body[0].name.should.equal('Suits')
-        res.body[0].should.have.property('channel')
-        res.body[0].channel.should.equal('USA Network')
-        res.body[0].should.have.property('rating')
-        res.body[0].rating.should.equal(3)
-        res.body[0].should.have.property('explicit')
-        res.body[0].explicit.should.equal(false)
-        done()
+describe('API Routes', () => {
+  beforeEach((done) => {
+    knex.migrate.rollback()
+      .then(() => {
+        knex.migrate.latest()
+          .then(() => {
+            return knex.seed.run()
+              .then(() => {
+                done()
+              })
+          })
       })
   })
+  afterEach((done) => {
+    knex.migrate.rollback()
+      .then(() => done())
+  })
+  describe('GET /api/v1/shows', () => {
+    it('should return all shows', (done) => {
+      chai.request(server)
+        .get('/api/v1/shows')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.should.be.json
+          res.body.should.be.a('array')
+          res.body.length.should.equal(4)
+          res.body[0].should.have.property('name')
+          res.body[0].name.should.equal('Suits')
+          res.body[0].should.have.property('channel')
+          res.body[0].channel.should.equal('USA Network')
+          res.body[0].should.have.property('rating')
+          res.body[0].rating.should.equal(3)
+          res.body[0].should.have.property('explicit')
+          res.body[0].explicit.should.equal(false)
+          done()
+        })
+    })
+  })
+
 })
